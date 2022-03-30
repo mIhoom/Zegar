09f993c47ce77724f4614f4237dc21ef8c4c4225
mIhoom <mdmichalski.poczta@gmail.com>
2022-03-30 17:21:27 +0200

    upload

---
+++ zegar.py
@@ -0,0 +1,24 @@
+import tkinter
+import time
+
+def wyswietlanie():
+    godz1 =''
+    obecna = time.strftime('%H:%M:%S')
+    if obecna != godz1:
+            godz1 = obecna
+            l2.config(text = obecna)
+    l2.after(200, wyswietlanie)
+
+root = tkinter.Tk()
+root.title("Zegar")
+root.geometry('340x170')
+
+l = tkinter.Label(root, text = 'Aktualna godzina: ', font = ('Calibri', 18), width = '20')
+l.pack()
+l2 = tkinter.Label(root, text = wyswietlanie, font=('Calibri', 30) ,bg = 'black', fg = 'green', height = '1', width = '10')
+l2.place(x = 65, y = 50)
+
+ 
+
+wyswietlanie()
+root.mainloop()
